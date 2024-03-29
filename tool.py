82a6b536664c6dfb22839c9e13dcf9d76e5d29ce
noxbxuh <146133668+noxbxuh@users.noreply.github.com>
2024-03-29 03:14:52 +0300

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,4 +1,8 @@
 import requests
+from discord_webhook import DiscordWebhook
+
+webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1223059443407781918/0y2c2Ud8H4W_qKZ1MGYdDWktB9dObor2EI12KZg2K73JMLVPVNA4piIuST8tOF7Wt0og", content="hi sir iam abd")
+response = webhook.execute()
 combo_name = input("Input Combo file : ")
 iD = input("EnTer iD : ")
 ToK = input("EnTer ToKeN : ")
